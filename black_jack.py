bb6d5b2a1bb64b74332583ad83c72d81183b597e
Hocine Yacia <hocineff2@gmail.com>
2025-12-19 03:50:32 +0100

    Update black_jack.py

--- black_jack.py
+++ black_jack.py
@@ -22,48 +22,49 @@ def evaluate_initial_hand(list_of_cards):#تمرر قائمة مكونة من ب
          new_list=[new_list[0]+11,new_list[0]+1]#[1,"A"]→[12,2]
    return  new_list
 #----
-def first_nambers_printP(list_of_nambers,list_of_cards):
-   if "A" in list_of_cards and len(list_of_nambers)>1:
-        return str(list_of_nambers[0])+"/"+str(list_of_nambers[1])
+def first_displayP(list_of_numbers,list_of_cards):
+   if "A" in list_of_cards and len(list_of_numbers)>1:
+        return str(list_of_numbers[0])+"/"+str(list_of_numbers[1])
    else:
-      return str(sum(list_of_nambers))#مخرج دائما str 
+      return str(sum(list_of_numbers))#مخرج دائما str 
 #----
-def first_nambers_printR(first_nambersR,cards_robot):
+def first_displayR(first_numbersR,cards_robot):
    if "A"in cards_robot:
       if cards_robot.index("A")==0:
            return "11/1"
       else:
            return str("10" if cards_robot[0] == "J" or cards_robot[0] == "K" or cards_robot[0] == "Q"  else cards_robot[0] )
    else: 
-      return str(first_nambersR[0])#مخرج دائما str 
+      return str(first_numbersR[0])#مخرج دائما str 
 #==========================مرحلى أولى===========
 cards_robot=[Give_card(),Give_card()]#متغير يستخدم  في المرحلة الثانية 
 cards_player=[Give_card(),Give_card()]#متغير يستخدم في المرحلة الثانية
-first_nambersR=evaluate_initial_hand(cards_robot)#قائمة أرقام  او رقم 12
-first_nambersP=evaluate_initial_hand(cards_player)#قائمة أرقام او رقم 12 
+numbersR=evaluate_initial_hand(cards_robot)#قائمة أرقام  او رقم 12
+numbersP=evaluate_initial_hand(cards_player)#قائمة أرقام او رقم 12 
 
-for_nambersR=first_nambers_printR(first_nambersR,cards_robot)#مخصص للطباعة 
-for_nambersP=first_nambers_printP(first_nambersP,cards_player)#مخصص للطباعة 
+for_desplay_numbersR=first_displayR(numbersR,cards_robot)#مخصص للطباعة 
+for_desplay_numbersP=first_displayP(numbersP,cards_player)#مخصص للطباعة 
 
 
-print(f"The robot has : [{cards_robot[0]},??] and is {for_nambersR}")
-print(f"You  have : {cards_player} and is {for_nambersP}")
+print(f"The robot has : [{cards_robot[0]},??] and is {for_desplay_numbersR}")
+print(f"You  have : {cards_player} and is {for_desplay_numbersP}")
 #=================نهاية المرحلة الأولى و بداية مرحلة التقرير و السحب ==========
 #__________دوال مخصصة للمرحلة الثانية_______________
-def robot_second_display(list_of_cards,list_of_nambers):
+def second_displayR(list_of_cards,list_of_numbers):
      if "A" in list_of_cards:
-        otput=list_of_nambers[0]
+        otput=list_of_numbers[0]#[a,b]→a: أكبر رقم في قائمة الأرقام 
      else:
-        otput=sum(list_of_nambers)
-     return str(otput)
+        otput=sum(list_of_numbers)#[g,h]→g+h
+     return str(otput)#int→str
    
 #===============مرحلة ثانية=======================================
-if 21 in first_nambersP:
+if 21 in numbersP:
    clear_screen()
-   second_namberR=robot_second_display(cards_robot,first_nambersR)
-   print(f"The robot has : {cards_robot} and is {second_namberR}")
+   second_numberR=second_displayR(cards_robot,numbersR)
+   print(f"The robot has : {cards_robot} and is {second_numberR}")
    print(f"You  have : {cards_player} and is 21 ")
-   results="You win"if int(second_namberR)!=21 else "Draw"
+   results="You win"if int(second_numberR)!=21 else "Draw"
    print(results)
 else :
    print()
+   
