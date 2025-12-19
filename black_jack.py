55b34670479fb43035042f54439b29a77d508a3f
Hocine Yacia <hocineff2@gmail.com>
2025-12-19 01:09:19 +0100

    Refactor evaluate_initial_hand for clarity

--- black_jack.py
+++ black_jack.py
@@ -8,19 +8,19 @@ def Give_card():#سحب بطاقة
 def clear_screen():#تنظيف شاشة 
    os.system("clear")
 #___________دوال مرحلة اولى____________________________________
-def evaluate_initial_hand(list_of_cards):#تمرر قائمة مكونة من عنصرين 
+def evaluate_initial_hand(list_of_cards):#تمرر قائمة مكونة من بطاقتين 
    new_list=[]
    for x in list_of_cards:
       new_card=10 if x=="J" or x=="K" or x=="Q"  else x  
-      new_list.append(new_card)
-   if "A" in list_of_cards:
-      if "A" == list_of_cards[0] and "A" == list_of_cards[1]:
-         new_list=[12]
-      elif new_list.index("A")==0:#اكبر يمين أصغر يسار 
-         new_list=[new_list[1]+11,new_list[1]+1]
-      else:#اكبر يمين أصغر يسار 
-         new_list=[new_list[0]+11,new_list[0]+1]
-   return  new_list#مخرج يكون قائمة فيها أرقام أو رقم حسب الحال  
+      new_list.append(new_card)#["K" ,4]→[10,4] | "A"→"A"
+   if "A" in new_list:
+      if "A" == new_list[0] and "A" == new_list[1]:
+         new_list=[12]#["A","A"]→[12]
+      elif new_list.index("A")==0:#اكبر يسار أصغر يمين
+         new_list=[new_list[1]+11,new_list[1]+1]#["A",1]→[12,2]
+      else: 
+         new_list=[new_list[0]+11,new_list[0]+1]#[1,"A"]→[12,2]
+   return  new_list
 #----
 def first_nambers_printP(list_of_nambers,list_of_cards):
    if "A" in list_of_cards and len(list_of_nambers)>1:
@@ -37,10 +37,10 @@ def first_nambers_printR(first_nambersR,cards_robot):
    else: 
       return str(first_nambersR[0])#مخرج دائما str 
 #==========================مرحلى أولى===========
-cards_robot=[Give_card(),Give_card()]#متغير عام يستعامل في المرحلة الثانية 
-cards_player=[Give_card(),Give_card()]#متغير عام يستخدم في المرحلة الثانية
-first_nambersR=evaluate_initial_hand(cards_robot)#قائمة أرقام 
-first_nambersP=evaluate_initial_hand(cards_player)#قائمة أرقام 
+cards_robot=[Give_card(),Give_card()]#متغير يستخدم  في المرحلة الثانية 
+cards_player=[Give_card(),Give_card()]#متغير يستخدم في المرحلة الثانية
+first_nambersR=evaluate_initial_hand(cards_robot)#قائمة أرقام  او رقم 12
+first_nambersP=evaluate_initial_hand(cards_player)#قائمة أرقام او رقم 12 
 
 first_nambersR=first_nambers_printR(first_nambersR,cards_robot)#مخصص للطباعة 
 for_nambersP=first_nambers_printP(first_nambersP,cards_player)#مخصص للطباعة 
@@ -49,3 +49,19 @@ for_nambersP=first_nambers_printP(first_nambersP,cards_player)#مخصص للطب
 print(f"The robot has : [{cards_robot[0]},??] and is {first_nambersR}")
 print(f"You  have : {cards_player} and is {for_nambersP}")
 #=================نهاية المرحلة الأولى و بداية مرحلة التقرير و السحب ==========
+#__________دوال مخصصة للمرحلة الثانية_______________
+def secend_number_printR(list_of_cards,list_of_nambers):
+     if "A" in list_of_cards:
+        otput=list_of_nambers[0]
+     else:
+        otput=sum(list_of_nambers)
+     return  otput
+   
+#===============مرحلة ثانية=======================================
+if 21 in first_nambersP:
+   clear_screen()
+   second_namberR=secend_number_printR(cards_robot,first_nambersR)
+   print(f"The robot has : {cards_robot} and is {second_namberR}")
+   print(f"You  have : {cards_player} and is 21 ")
+else :
+   print()
